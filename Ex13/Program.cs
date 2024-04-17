723f0d252676872e834fbcd90249084eb4e6ada9
Anael-Fer <166033786+Anael-Fer@users.noreply.github.com>
2024-04-17 06:18:19 -0300

    Add files via upload

---
+++ Ex13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ano;
+
+            Console.WriteLine("Digite um ano:");
+            ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ano: ");
+
+            Console.WriteLine((ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)) ? "é bissexto" : "não é bissexto");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ex9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num_extenso;
+
+
+            Console.WriteLine("Escreva um número:");
+            num_extenso = int.Parse(Console.ReadLine());
+
+            switch (num_extenso)
+            {
+                case 0:
+                    Console.WriteLine("Zero");
+                   break;
+                case 1:
+                    Console.WriteLine("Um");
+                    break;
+                case 2:
+                    Console.WriteLine("Dois");
+                    break;
+                case 3:
+                    Console.WriteLine("Três");
+                    break;
+                case 4:
+                    Console.WriteLine("Quatro");
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Número inválido!");
+                    }
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
